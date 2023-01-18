3d49ec35f3c7129dedf1f7adeda80c82a2de243a
khuephamy <khuephumy19@gmail.com>
2023-01-18 16:47:53 -0600

    Close and open modal

--- scripts/index.js
+++ scripts/index.js
@@ -25,3 +25,44 @@ const initialCards = [
   },
 ];
 
+/* Variables */
+
+const profileEditButton = document.querySelector('.profile__edit');
+const profileEditModal = document.querySelector('.modal');
+const modalClose = document.querySelector('.modal__close');
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const profileTitleInput = document.querySelector('#profile__title-input');
+const profileDescriptionInput = document.querySelector('#profile__description-input');
+const submitButton = document.querySelector('.modle__form_save');
+
+
+// Functions
+
+function openProfileModal() {
+  profileEditModal.classList.add("modal__opened");
+}
+
+function closeProfileModal() {
+  profileEditModal.classList.remove("modal__opened");
+}
+
+
+function openProfileEditForm() {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  openProfileModal();
+}
+
+function profileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closeProfileModal();
+}
+
+// Event Listener
+
+profileEditButton.addEventListener('click', openProfileEditForm);
+modalClose.addEventListener("click", closeProfileModal);
+submitButton.addEventListener("submit", profileEditSubmit);
