a055d6a156ef0661a57dabf2a2f5324706beae40
khuephamy <khuephumy19@gmail.com>
2023-03-10 19:47:05 -0600

    delete inputValues Variable

--- src/components/popupWithForm.js
+++ src/components/popupWithForm.js
@@ -1,7 +1,7 @@
 import Popup from "./Popup";
 
 export default class PopupWithForm extends Popup {
-  constructor({popupSelector, handleFormSubmit}) {
+  constructor({ popupSelector, handleFormSubmit }) {
     super(popupSelector);
     this._modalForm = this._popupElement.querySelector(".modal__form");
     this._formInputs = this._modalForm.querySelectorAll(".modal__form-input");
@@ -27,8 +27,7 @@ export default class PopupWithForm extends Popup {
 
     this._modalForm.addEventListener("submit", (e) => {
       e.preventDefault();
-      const inputValues = this._getInputValues();
-      this._handleFormSubmit(inputValues);
+      this._handleFormSubmit(this._getInputValues());
       this.close();
     });
   }
