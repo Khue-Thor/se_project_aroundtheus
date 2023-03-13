831e4a006d63d6e092d994befa408da0e64b37b2
khuephamy <khuephumy19@gmail.com>
2023-03-13 14:47:44 -0500

    create handleRes/handleResError

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,5 +1,19 @@
 export default class Api {
-  constructor() {
-    
+  constructor(baseUrl, authToken, headers ) {
+    this._baseUrl = baseUrl;
+    this._authTokenn = authToken;
+    this._headers = headers;
   }
+
+  _handleResponse(res) {
+    return res.ok 
+      ? res.json()
+      : Promise.reject(`Error: ${res.status} ${res.statusText}`);
+  }
+
+  _handleResponseError(err) {
+    console.log(`Error processing request ${err}`);
+  }
+
+  // GET: get App Data (cardList, userData)
 }
