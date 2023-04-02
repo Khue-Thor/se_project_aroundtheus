45ffd8cd2a3b3e77188473d267663c1a34a36a78
khuephamy <khuephumy19@gmail.com>
2023-04-02 09:56:20 -0500

    cards styles updated

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,7 @@
 export default class Section {
   constructor({ items, renderer }, containerSelector) {
-    this._renderer = renderer;
     this._items = items;
+    this._renderer = renderer;
     this._container = document.querySelector(containerSelector);
   }
 
